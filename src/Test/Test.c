84373bef70cf9e829597d234d34a3673a26dd699
HOMEPC\alex3696 <alex3696@yandex.ru>
2026-07-11 20:47:51 +0700

    FIX/C - добавил проверку размера схемы для x64

--- src/Test/Test.c
+++ src/Test/Test.c
@@ -300,9 +300,20 @@ static int WriteSample(EdfContext_t* dw)
 	};
 #pragma pack(pop)
 
-	int len0 = GetEdfSchemaCBinLen(&keyValueType);
-	if (93 != len0)
-		return ERR_BASE;
+	size_t len0 = GetEdfSchemaCBinLen(&keyValueType);
+	if (4 == sizeof(void*))
+	{
+		assert(93 == len0);
+		if (93 != len0)
+			return ERR_BASE;
+	}
+	else if (8 == sizeof(void*))
+	{
+		assert(137 == len0);
+		if (137 != len0)
+			return ERR_BASE;
+	}
+		
 	err = EdfWriteSchema(dw, &keyValueType, &writed);
 	EdfWriteData(dw, &((KeyValue_t) { "Key1", "Value1" }), sizeof(KeyValue_t), &consumed);
 	EdfWriteData(dw, &((KeyValue_t) { "Key2", "Value2" }), sizeof(KeyValue_t), &consumed);
