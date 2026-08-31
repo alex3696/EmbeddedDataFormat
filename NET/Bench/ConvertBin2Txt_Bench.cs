cba30198764b0e76f8324525c1206eaee0858498
SAVPC\Sav <alex3696@yandex.ru>
2026-08-31 14:39:10 +0700

    FEAT NET - оптимизированы конвертеры и их тесты

--- NET/Bench/ConvertBin2Txt_Bench.cs
+++ NET/Bench/ConvertBin2Txt_Bench.cs
@@ -1,11 +1,6 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
-using EdfNet.Converters;
-using EdfNet.Core;
-using EdfNet.Interfaces;
 using NetTest;
-using System;
-using System.IO;
 
 namespace Bench;
 
@@ -17,6 +12,14 @@ namespace Bench;
 |                  |                |         |       |           |             |
 | 'Text -> Binary' | .NET 10.0      | 1.997 s |  1.00 |  14.64 KB |        1.00 |
 | 'Text -> Binary' | NativeAOT 10.0 | 2.515 s |  1.26 |  26.05 KB |        1.78 |
+
+| Method           | Runtime        | Mean    | Ratio | Allocated | Alloc Ratio |
+|----------------- |--------------- |--------:|------:|----------:|------------:|
+| 'Binary -> Text' | .NET 10.0      | 1.606 s |  1.00 |  16.32 KB |        1.00 |
+| 'Binary -> Text' | NativeAOT 10.0 | 2.433 s |  1.51 |  26.38 KB |        1.62 |
+|                  |                |         |       |           |             |
+| 'Text -> Binary' | .NET 10.0      | 2.376 s |  1.00 |  14.28 KB |        1.00 |
+| 'Text -> Binary' | NativeAOT 10.0 | 3.073 s |  1.29 |  23.77 KB |        1.66 |
  */
 
 [MemoryDiagnoser(true)]
@@ -26,74 +29,27 @@ namespace Bench;
 [SimpleJob(warmupCount: 0, iterationCount: 1, invocationCount: 1, launchCount: 1, runtimeMoniker: RuntimeMoniker.NativeAot10_0, baseline: false)]
 public class ConvertBin2Txt_Bench
 {
-    public const int NCOUNT = 1_000_000;
-    public static readonly string FileName = "ConvertTest";
 #pragma warning disable CS8618
-    private string _binFile;
-    private string _txtFile;
-    private string _tempFile;
+    TestConverters _сonverter = new();
 #pragma warning restore CS8618
     [GlobalSetup]
     public void Setup()
     {
-        _binFile = TestStructSerialize.GetTestFilePath($"{FileName}.bdf");
-        _txtFile = TestStructSerialize.GetTestFilePath($"{FileName}.tdf");
 
-        // Создаем тестовые файлы если их нет
-        if (!File.Exists(_binFile))
-            CreateFile(_binFile, st => new EdfBinaryWriter(st));
-
-        if (!File.Exists(_txtFile))
-            CreateFile(_txtFile, st => new EdfTextWriter(st));
-    }
-    private void CreateFile(string fileName, Func<Stream, IWriter> factory)
-    {
-        using var file = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.Read);
-        var writer = factory.Invoke(file);
-        writer.WriteSchema(TestClasses_Content.KeyValSchema);
-
-        for (int i = 0; i < NCOUNT; i++)
-            writer.WriteValue(TestClasses_Content.TestValue);
-
-        writer.Flush();
-        if (writer is IDisposable d)
-            d.Dispose();
     }
 
     [Benchmark(Description = "Binary -> Text")]
     public void BinToTxtConvert()
     {
-        _tempFile = TestStructSerialize.GetTestFilePath($"{FileName}_temp_{Guid.NewGuid()}.tdf");
-        try
-        {
-            BinToTxt.Convert(_binFile, _tempFile);
-
-            // Проверка (опционально, можно закомментировать для скорости)
-            // bool isEqual = FileUtils.FileCompare(_txtFile, _tempFile);
-            // if (!isEqual) throw new Exception("Files not equal");
-        }
-        finally
-        {
-            // Очистка
-            try { if (File.Exists(_tempFile)) File.Delete(_tempFile); } catch { }
-        }
+        _сonverter.BinToTxtConvert();
     }
 
     [Benchmark(Description = "Text -> Binary")]
     public void TxtToBinConvert()
     {
-        _tempFile = TestStructSerialize.GetTestFilePath($"{FileName}_temp_{Guid.NewGuid()}.bdf");
-        try
-        {
-            TxtToBin.Convert(_txtFile, _tempFile);
-
-            // bool isEqual = FileUtils.FileCompare(_binFile, _tempFile);
-            // if (!isEqual) throw new Exception("Files not equal");
-        }
-        finally
-        {
-            try { if (File.Exists(_tempFile)) File.Delete(_tempFile); } catch { }
-        }
+        _сonverter.TxtToBinConvert();
     }
+    [GlobalCleanup]
+    public void DeleteConvertedFiles() => _сonverter.DeleteConvertedFiles();
 }
 
--- NET/NetTesting/TestConverters.cs
+++ NET/NetTesting/TestConverters.cs
@@ -10,16 +10,17 @@ public class TestConverters
 {
     public static readonly string FileName = "ConvertTest";
     public const int NCOUNT = 1_000_000;
-    readonly string _binFile;
-    readonly string _txtFile;
+    public readonly string _binFile = TestStructSerialize.GetTestFilePath($"{FileName}.bdf");
+    public readonly string _txtFile = TestStructSerialize.GetTestFilePath($"{FileName}.tdf");
+    public readonly string _binFileConv = TestStructSerialize.GetTestFilePath($"{FileName}Conv.bdf");
+    public readonly string _txtFileConv = TestStructSerialize.GetTestFilePath($"{FileName}Conv.tdf");
+
     public TestConverters()
     {
-        _binFile = TestStructSerialize.GetTestFilePath($"{FileName}.bdf");
-        _txtFile = TestStructSerialize.GetTestFilePath($"{FileName}.tdf");
         CreateBinFilesIfNotExist(_binFile, NCOUNT, st => new EdfBinaryWriter(st));
         CreateBinFilesIfNotExist(_txtFile, NCOUNT, st => new EdfTextWriter(st));
     }
-    void CreateBinFilesIfNotExist(string fileName, int count, Func<Stream, IWriter> factory)
+    static void CreateBinFilesIfNotExist(string fileName, int count, Func<Stream, IWriter> factory)
     {
         if (File.Exists(fileName))
             return;
@@ -34,20 +35,36 @@ public class TestConverters
         if (_writerGen is IDisposable d)
             d.Dispose();
     }
-    public void BinToTxtConvert() => BinToTxt.Convert(_binFile, TestStructSerialize.GetTestFilePath($"{FileName}Conv.tdf"));
-    public void TxtToBinConvert() => TxtToBin.Convert(_txtFile, TestStructSerialize.GetTestFilePath($"{FileName}Conv.bdf"));
+    public void DeleteConvertedFiles()
+    {
+        try
+        {
+            if (File.Exists(_binFileConv))
+                File.Delete(_binFileConv);
+        }
+        catch { }
+        try
+        {
+            if (File.Exists(_txtFileConv))
+                File.Delete(_txtFileConv);
+        }
+        catch { }
+    }
+
+    public void BinToTxtConvert() => BinToTxt.Convert(_binFile, _txtFileConv);
+    public void TxtToBinConvert() => TxtToBin.Convert(_txtFile, _binFileConv);
     [TestMethod]
     public void TestBinToTxtConvert()
     {
         {
             using var src = new FileStream(_binFile, FileMode.Open, FileAccess.Read);
-            using var dst = new FileStream(TestStructSerialize.GetTestFilePath($"{FileName}Conv.tdf"), FileMode.Create, FileAccess.Write);
+            using var dst = new FileStream(_txtFileConv, FileMode.Create, FileAccess.Write);
             RunSingleTest("Bin >> Txt", () =>
             {
                 BinToTxt.Convert(src, dst);
             });
         }
-        bool isEqual = FileUtils.FileCompare(_txtFile, TestStructSerialize.GetTestFilePath($"{FileName}Conv.tdf"));
+        bool isEqual = FileUtils.FileCompare(_txtFile, _txtFileConv);
         Assert.IsTrue(isEqual);
     }
     [TestMethod]
@@ -55,13 +72,13 @@ public class TestConverters
     {
         {
             using var src = new FileStream(_txtFile, FileMode.Open, FileAccess.Read);
-            using var dst = new FileStream(TestStructSerialize.GetTestFilePath($"{FileName}Conv.bdf"), FileMode.Create, FileAccess.Write);
+            using var dst = new FileStream(_binFileConv, FileMode.Create, FileAccess.Write);
             RunSingleTest("Txt >> Bin", () =>
             {
                 TxtToBin.Convert(src, dst);
             });
         }
-        bool isEqual = FileUtils.FileCompare(_binFile, TestStructSerialize.GetTestFilePath($"{FileName}Conv.bdf"));
+        bool isEqual = FileUtils.FileCompare(_binFile, _binFileConv);
         Assert.IsTrue(isEqual);
     }
 
